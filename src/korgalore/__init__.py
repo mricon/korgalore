1d39f0931b6a2d93fb2c367b98a0baec506df3be
Konstantin Ryabitsev <konstantin@linuxfoundation.org>
2026-01-20 16:09:01 -0500

    Bump version to 0.5-dev
    Signed-off-by: Konstantin Ryabitsev <konstantin@linuxfoundation.org>

--- src/korgalore/__init__.py
+++ src/korgalore/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from typing import List, Optional, Tuple
 
-__version__ = "0.4"
+__version__ = "0.5-dev"
 __author__ = "Konstantin Ryabitsev"
 __email__ = "konstantin@linuxfoundation.org"
 
